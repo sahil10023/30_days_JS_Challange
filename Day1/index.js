7b528e20d3653a1b9c2302d192c005c63753850d
Sahil10023 <81016048+sahil10023@users.noreply.github.com>
2024-07-14 09:57:23 +0530

    Adding files for day 1 and day 2 Challange

---
+++ Day1/index.js
@@ -0,0 +1,44 @@
+// Activity 1
+
+//task 1
+var a = 34;
+console.log(a);
+//task 2
+let b = "Chai Aur Code";
+console.log(b);
+
+// Activity 2
+
+//task 3
+const result = true;
+console.log(result);
+
+// Activity 3
+
+let var1 = 45;
+let var2 = "Chai our Code";
+let var3 = false;
+let var4 = {"Number" : 45};
+let var5 = [35,67,98];
+
+console.log(typeof(var1));
+console.log(typeof(var2));
+console.log(typeof(var3));
+console.log(typeof(var4));
+console.log(typeof(var5));
+// array is an type of object in javaScript 
+
+// Activity 4
+
+let value = 15;
+console.log(value);
+value=21;
+console.log(value);
+
+// Activity 5
+
+const num = 45;
+// num = 56;
+// console.log(num)
+
+// TypeError : Assignment to constant variable.
---
+++ Day2/index.js
@@ -0,0 +1,54 @@
+// Activity 1
+
+// Task1
+let a = 10;
+let b = 20;
+console.log(a+b);
+// Task2
+console.log(b-a);
+// Task3
+console.log(a*b);
+// Task4
+console.log(b/a);
+// Task5
+console.log(b%a);
+
+// Activity 2
+
+// Task1
+console.log(a+=b);
+// Task2
+console.log(a-=b);
+
+// Activity 3
+
+// Task1
+console.log(a>b);
+console.log(a<b);
+// Task2
+console.log(a>=b);
+console.log(a<=b);
+// Task3
+b=10;
+console.log(a==b);
+console.log(a===b);
+b=20;
+
+// Activity 4
+
+// Task1
+if(a &&b){
+    console.log("A and B exists");
+}
+// Task2
+if((a ||b) ==10){
+    console.log("A or B is 10");
+}
+// Task3
+if(a !=20){
+    console.log("A is not 20");
+}
+
+// Activity 5
+
+a>0 ?console.log("number Is Positive"): console.log("number Is nagative");
